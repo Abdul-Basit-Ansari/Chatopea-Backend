1dbe941a9aa03383f50a151588e7e8d28da6371c
Abdul-Basit-Ansari <abdulbasitansari288@gmail.com>
2023-02-26 20:17:21 +0500

    Update database.py
    Co-Authored-By: Abdul Basit Ansari <114910040+Abdul-Basit-Pro@users.noreply.github.com>

--- config/database.py
+++ config/database.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'mysql://root:****@localhost/chatopea')
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'mysql://root:3UXuLNiYoZEEXuWqyP2H@containers-us-west-118.railway.app:6416/railway')
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'mysql://root:****@localhost/chatopea')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
